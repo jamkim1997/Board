5967598863a24911d0f92c3988e5b2b93f044211
jamkim1997 <jamkim1997@gmail.com>
2023-05-29 20:57:56 +1000

    Form added

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -1,15 +1,45 @@
 package com.board.board.controller;
 
+import com.board.board.entity.Board;
+import com.board.board.repository.BoardRepository;
+import com.board.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    private final BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> allBoards = boardService.findAllBoards();
+        model.addAttribute("boards", allBoards);
         return "/board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board());
+        return "/board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute Board board) {
+        boardService.saveBoard(board);
+        return "redirect::/board/list";
     }
 }
---
+++ src/main/java/com/board/board/service/BoardService.java
@@ -0,0 +1,23 @@
+package com.board.board.service;
+
+import com.board.board.entity.Board;
+import com.board.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public List<Board> findAllBoards() {
+        return boardRepository.findAll();
+    }
+    public Board saveBoard(Board board) {return boardRepository.save(board);}
+}
