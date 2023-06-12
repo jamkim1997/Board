67b1ce5b0c03bab57db87360bb15d2628d571e3c
jamkim1997 <jamkim1997@gmail.com>
2023-06-12 15:21:58 +1000

    Transaction

--- src/main/java/com/board/board/service/UserService.java
+++ src/main/java/com/board/board/service/UserService.java
@@ -1,23 +1,31 @@
 package com.board.board.service;
 
+import com.board.board.entity.Board;
 import com.board.board.entity.Role;
 import com.board.board.entity.User;
+import com.board.board.repository.BoardRepository;
 import com.board.board.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private final BoardRepository boardRepository;
+
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, BoardRepository boardRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.boardRepository = boardRepository;
     }
 
+    @Transactional
     public User save(User user) {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
@@ -25,6 +33,14 @@ public class UserService {
         Role role = new Role();
         role.setId(1l);
         user.getRoles().add(role);
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        Board board = new Board();
+        board.setTitle("new! " +savedUser.getUsername());
+        board.setContent("Hello. I just joined");
+        board.setUser(savedUser);
+        boardRepository.save(board);
+
+        return savedUser;
     }
 }
